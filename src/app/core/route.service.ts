bd1dfd6e283ecfb4da94d94115300e88fd153ef9
aacevedoday <you@example.com>
2018-07-07 14:47:10 -0500

    unable authentication

--- src/app/core/route.service.ts
+++ src/app/core/route.service.ts
@@ -18,7 +18,7 @@ export class Route {
       path: '',
       component: ShellComponent,
       children: routes,
-      canActivate: [AuthenticationGuard],
+      // canActivate: [AuthenticationGuard],
       // Reuse ShellComponent instance when navigating between child views
       data: { reuse: true }
     };
